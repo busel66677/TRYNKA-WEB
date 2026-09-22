b9924bd2d7f7f9436c0e28709fc856a315fdcd01
busel66677 <nuino666@gmail.com>
2026-09-22 21:21:31 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Після створення Supabase-проєкту встав сюди 2 значення з Project Settings → API.
+window.TRYNKA_CONFIG = {
+  supabaseUrl: "https://oqyjypzltncbruzaxetp.supabase.co",
+  supabaseAnonKey: "sb_publishable_PUAdJ6qJ6U8nf0UrK1FmKQ_PcyMEjIZ"
+};
